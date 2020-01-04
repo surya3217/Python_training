90b5c436e9dee9604525d452ff46ef889757f6b6
surya3217 <2015kuec2015@iiitkota.ac.in>
2020-01-04 23:14:21 +0530

    Added new file

---
+++ mileage_cal.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  3 14:55:37 2020
+
+@author: Windows10
+"""
+
+"""
+Name: 
+    Gas Mileage Calculator         
+Filename:
+    mileage_cal.py
+Problem Statement:
+    Assume my car travels 100 Kilometres after putting 5 litres of fuel. 
+    Calculate the average of my car.
+Data:
+    Not required
+Extension:
+    Take the temperarure of the city from input 
+Hint: 
+    Divide kilmeters by the litres used to get the average
+Algorithm:
+    Not Available  
+Boiler Plate Code:
+    Not Available 
+Sample Input:
+    Not Available 
+Sample Output:
+    Not Available 
+"""
+
+fuel= float(input('enter fuel in litres: '))
+dist= float(input('enter distance travelled in km: '))
+print('Average of my car: ',dist/fuel,'km/lit5')
+
+
