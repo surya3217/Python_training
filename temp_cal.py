e0bb96e4e1fd0b93e460fff6e87435b3ce43f647
surya3217 <2015kuec2015@iiitkota.ac.in>
2020-01-10 15:18:40 +0530

    Added a new file

--- temp_cal.py
+++ temp_cal.py
@@ -1,16 +1,11 @@
-# -*- coding: utf-8 -*-
 """
-Name: 
-    Temperature Calculator         
+Name: Temperature Calculator         
 Problem Statement:
     Assume today's temperature in Jaipur is 29 degree Centigrade. 
     Calculate the temperate in Degree Fahrenheit and print it.
-    Calculate the temperature in Degree Kelvin and print it. 
-Hint: 
-    Multiply by 9/5 and add 32  to get in Fahrenheit
-    Add 273 approximately to get in Kelvin
+    Calculate the temperature in Degree Kelvin and print it.
 """
 
-temp= float(input('enter the temperatute of city in degree Centigrade: '))
-print('temperate in Degree Fahrenheit: ',(temp*9)/5 +32,chr(176),'F',sep='')
-print('temperature in Degree Kelvin: ',temp+273,'K')
+temp= float(input('Enter the temperatute of city in degree Centigrade: '))
+print('Temperate in Degree Fahrenheit: ',(temp*9)/5 +32,chr(176),'F',sep='')
+print('Temperature in Degree Kelvin: ',temp+273,'K')
